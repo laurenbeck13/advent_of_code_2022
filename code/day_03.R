6d8e292be0a3915a3336f172e5f39bbdd691d666
Lauren Beck <lauren.beck@pathos.com>
2022-12-06 10:39:19 -0500

    last commit had bug

--- code/day_03.R
+++ code/day_03.R
@@ -4,14 +4,17 @@ data <- read.csv("data/day_03.txt",
                  header = FALSE, 
                  sep = " ")
 
-result_1 <- data %>%
+data_result_1 <- data %>%
   dplyr::mutate(compartment_1 = substr(V1, 1, nchar(V1) / 2),
                 compartment_2 = substr(V1, nchar(V1) / 2 + 1, nchar(V1)),
                 list_1 = strsplit(compartment_1, split = NULL),
                 list_2 = strsplit(compartment_2, split = NULL))
 
-result_1$common <- Map(intersect, result_1$list_1, result_1$list_2)
+data_result_1$common <- Map(intersect, data_result_1$list_1, data_result_1$list_2)
 
-result_1$priorty <- match(result_1$common, 
+data_result_1$priorty <- match(data_result_1$common, 
                           c(letters[1:26], 
                             LETTERS[1:26]))
+
+result_1 <- sum(data_result_1$priorty)
+
